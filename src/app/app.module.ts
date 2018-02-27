0cbce163320ede5c74f4c6d534cb61cfc83b8415
unknown <elsete79@gmail.com>
2018-02-27 12:15:46 +0100

    Clase 54

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//Rutas
+// Rutas
 import { APP_ROUTING } from './app.routes';
 
-//Servicios
-import  {HeroesService} from './servicios/heroes.service';
+// Servicios
+import { HeroesService } from './servicios/heroes.service';
 
 
-//Componentes
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
+
 export class HeroesComponent implements OnInit {
 
   public heroes:Heroe[] = [];
 
-  constructor(private _heroesService:HeroesService) { 
-        
+  constructor(private _heroesService:HeroesService, private _router:Router) { 
   }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.heroes = this._heroesService.getHeroes()
+  }
 
-    this.heroes = this._heroesService.getHeroes();
-
+  public verHeroe(id:number){ 
+    this._router.navigate(['/heroe', id]);
   }
 
 }
